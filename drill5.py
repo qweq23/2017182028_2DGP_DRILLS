decdda28367b2c8c70f24e3342e08a868d26435d
qweq23 <33680108+qweq23@users.noreply.github.com>
2019-10-09 14:55:56 +0900

    move character

--- drill5.py
+++ drill5.py
@@ -6,12 +6,21 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 def handle_events():
    global running
    global x, y
+   global move
+   global mouse_x, mouse_y
+   global pre_x, pre_y
+
    events = get_events()
    for event in events:
        if event.type == SDL_QUIT:
            running = False
        elif event.type == SDL_MOUSEMOTION:
            x, y =  event.x, KPU_HEIGHT - 1 - event.y
+       elif event.type == SDL_MOUSEBUTTONDOWN:
+           move = True
+           mouse_x, mouse_y = event.x, KPU_HEIGHT - 1 - event.y
+           pre_x = ch_x
+           pre_y = ch_y
        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
            running = False
 
@@ -37,7 +46,20 @@ def draw():
 
     hand_arrow.draw(x, y, 50, 50)
 
-
+def update_character_position():
+    global move
+    global mouse_x, mouse_y
+    global pre_x, pre_y
+    global ch_x, ch_y
+    global t
+    if move == True:
+            t = t + 0.01
+            ch_x = (1 - t) * pre_x + t * mouse_x
+            ch_y = (1 - t) * pre_y + t * mouse_y
+            if t > 1:
+                move = False
+                t = 0
+                
 
 
 
@@ -52,9 +74,11 @@ ch_x, ch_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
 move = False
 ch_dir = True   #true면 오른쪽 false면 왼쪽
-
-
-
+t = 0.0
+mouse_x = 0
+mouse_y = 0
+pre_x = 0
+pre_y = 0
 hide_cursor()
 
 while running:
@@ -65,7 +89,7 @@ while running:
     draw()
     update_canvas()
     frame = (frame + 1) % 8
-
+    update_character_position()
     handle_events()
 
 close_canvas()
