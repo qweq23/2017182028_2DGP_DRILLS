ce064b15261993db5d94b2731012b05edc056be4
qweq23 <33680108+qweq23@users.noreply.github.com>
2019-10-11 11:49:28 +0900

    drill-06-2 완료
    2017182028 윤혜림 drill-06-2
    커밋 횟수 2번
    https://www.youtube.com/watch?v=3BojDJQGmrs

--- Drill-06/drill-06-2.py
+++ Drill-06/drill-06-2.py
@@ -1,6 +1,18 @@
 from pico2d import *
 import random as r
 
+
+def handle_events():
+   global running
+
+   events = get_events()
+   for event in events:
+       if event.type == SDL_QUIT:
+           running = False
+       elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+           running = False
+
+
 def draw():
     global frame
     global ch_x, ch_y
@@ -13,8 +25,23 @@ def draw():
         character.clip_draw(frame * 100, 100 * 0, 100, 100, ch_x, ch_y)
 
 
-def update_character_position(p1, p2, p3, p4):
-    pass
+def update_character_position(p1, p2, p3, p4, i):
+    global points
+    global ch_x, ch_y
+    global look_right
+
+    t = i / 100
+    x = ((-t ** 3 + 2 * t ** 2 - t) * p1[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[0] +
+          (-3 * t ** 3 + 4 * t ** 2 + t) * p3[0] + (t ** 3 - t ** 2) * p4[0]) / 2
+    y = ((-t ** 3 + 2 * t ** 2 - t) * p1[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[1] +
+          (-3 * t ** 3 + 4 * t ** 2 + t) * p3[1] + (t ** 3 - t ** 2) * p4[1]) / 2
+    if ch_x < x:
+        look_right = True
+    else:
+        look_right = False
+
+    ch_x = x
+    ch_y = y
 
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
@@ -22,6 +49,7 @@ open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 
+running = True
 frame = 0
 look_right = True
 size = 10
@@ -30,15 +58,16 @@ n = 1
 ch_x, ch_y = points[n - 1]
 i = 0
 
-while True:
+while running == True:
     clear_canvas()
     draw()
     update_canvas()
     frame = (frame + 1) % 8
-    update_character_position(points[n - 3], points[n - 2], points[n - 1], points[n])
+    update_character_position(points[n - 3], points[n - 2], points[n - 1], points[n], i)
     i = (i + 2) % 100
     if i == 0:
         n = (n + 1) % size
+    handle_events()
 
     delay(0.03)
 
